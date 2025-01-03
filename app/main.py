7f5c49477ce6260ad6a6799373dfeaa2ee204bcf
maksim <knyazev.maksim2852@gmail.com>
2025-01-03 22:35:56 +0400

    setup di

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+from dishka import make_async_container, AsyncContainer
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-import uvicorn
+from app.ioc import AppProvider
+from app.routers.auth import auth_router
+from dishka.integrations import fastapi as fastapi_integration
+from app.config import Config
 
+
+
+config: Config = Config()
+container: AsyncContainer = make_async_container(AppProvider(), context={Config: config})
 
 
 @asynccontextmanager
@@ -17,8 +25,7 @@ def create_app() -> FastAPI:
         description='Это веб-приложение для управления личными финансами',
         lifespan=lifespan,
     )
+    app.include_router(router=auth_router, prefix='/api/v1/auth')
+    fastapi_integration.setup_dishka(container=container, app=app)
     return app
 
-
-if __name__ == '__main__':
-    uvicorn.run(create_app)
